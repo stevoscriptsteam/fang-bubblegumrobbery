c861e3c1c26f4a3bdba5a05dee948d02567e4440
StevoScripts <119699613+stevoscriptsteam@users.noreply.github.com>
2024-10-22 11:29:17 +1000

    add snowy event protection

--- server/server.lua
+++ server/server.lua
@@ -6,11 +6,17 @@ lib.callback.register('fang-bubblegumrobbery:server:getRobbedMachines', function
     return robbedMachines[machineEntity]
 end)
 
-lib.callback.register('fang-bubblegumrobbery:server:setRobbedStatus', function(_, entityID, state)
-    if robbedMachines[entityID] then return end
+lib.callback.register('fang-bubblegumrobbery:server:setRobbedStatus', function(source, entityID, state)
+    if robbedMachines[entityID] then
+        lib.print.warn('[WARNING] Player triggered giveMoney callback with robbed machine')
+        return false
+    end
+    if #(GetEntityCoords(cache.ped) - GetEntityCoords(entityID)) > config.Distance then
+        lib.print.warn('[WARNING] Player triggered giveMoney callback with invalid distance')
+        return false
+    end
 
     robbedMachines[entityID] = state
-   
     if state and not resetTimers[entityID] then
         resetTimers[entityID] = lib.timer((config.Cooldown * 1000), function()
             robbedMachines[entityID] = nil
@@ -25,22 +31,29 @@ lib.callback.register('fang-bubblerobbery:server:giveItem', function(source)
     local items = config.Items
     local itemSelect = math.random(1,5)
     local prizeItem = items[itemSelect].item
-    local moneyCount = exports.ox_inventory:GetItemCount(source, 'money')
-    local data = {
-        title = 'Error',
-        description = 'You are too broke...',
-        type = 'error'
-    }
-    if moneyCount < config.Price then
-        TriggerClientEvent('ox_lib:notify', source, data)
-        return 
+    if not exports.ox_inventory:RemoveItem(source, 'money', config.Price) then 
+        return TriggerClientEvent('ox_lib:notify', source, {
+            title = 'Error',
+            description = 'You are too broke...',
+            type = 'error'
+        })
     end
-    exports.ox_inventory:RemoveItem(source, 'money', config.Price)
     exports.ox_inventory:AddItem(source, prizeItem, items[itemSelect].count)
 end)
 
-lib.callback.register('fang-bubblerobbery:server:giveMoney', function(source)
-    local amount = math.random(10,30)
-    exports.ox_inventory:RemoveItem(source, 'lockpick', 1)
-    exports.ox_inventory:AddItem(source, 'money', amount)
+lib.callback.register('fang-bubblerobbery:server:giveMoney', function(source, entity)
+    if robbedMachines[entity] then
+        lib.print.warn('[WARNING] Player triggered giveMoney callback with robbed machine')
+        return false
+    end
+    if #(GetEntityCoords(cache.ped) - GetEntityCoords(entity)) > config.Distance then
+        lib.print.warn('[WARNING] Player triggered giveMoney callback with invalid distance')
+        return false
+    end
+    if not exports.ox_inventory:RemoveItem(source, 'lockpick', 1) then
+        lib.print.warn('[WARNING] Player triggered giveMoney callback without lockpick')
+        return false
+    end
+    exports.ox_inventory:AddItem(source, 'money', math.random(10,30))
+    return true
 end)
