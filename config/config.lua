fad1396e95a8d18ebfbc7cd4ab4da3f00241a4cb
StevoScripts <119699613+stevoscriptsteam@users.noreply.github.com>
2024-10-22 11:22:05 +1000

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,13 +1,14 @@
-Config = {}
+return {
+    Items = {
+        { item = "gum1", count = 1 },
+        { item = "gum2", count = 1 },
+        { item = "gum3", count = 1 },
+        { item = "gum4", count = 1 },
+        { item = "gum5", count = 1 }
+    },
+    
+    Price = 30,
+    
+    Cooldown = 90
 
-Config.Items = {
-    { item = "gum1", count = 1 },
-    { item = "gum2", count = 1 },
-    { item = "gum3", count = 1 },
-    { item = "gum4", count = 1 },
-    { item = "gum5", count = 1 },
 }
-
-Config.Price = 30
-
-Config.Cooldown = 90
